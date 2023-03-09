a00a1c6df0b6b5ea42cbb0641d6ed15fc6844230
Carlos Andres Garzon Gonzalez <carlos.garzon@mercadolibre.com.co>
2023-03-08 22:23:52 -0500

    Implement params from file

--- dags/dummy_1.py
+++ dags/dummy_1.py
@@ -9,15 +9,16 @@ with DAG(
     "dummy_1",
     start_date=pendulum.datetime(2022, 8, 21, tz="UTC"),
     schedule_interval=None,
-    default_args={"depends_on_past": False, "owner": "Carlos Garzon", "retries": 0},
+    default_args={"depends_on_past": False, "owner": "The User", "retries": 0},
     catchup=False,
     description="Dummy DAG",
-    tags=["version:0.1.1"],
-    params={"delay": Param("1", type=["string"])},
+    tags=["version:0.1.3"],
+    params={"delay": Param(1, type="integer", minimum=0)},
+    render_template_as_native_obj=True,
 ) as dag:
 
     @task(task_id="sleep", trigger_rule="all_done")
-    def my_sleeping_function(delay: str) -> None:
-        time.sleep(int(delay))
+    def my_sleeping_function(delay: int) -> None:
+        time.sleep(delay)
 
     sleeping_task = my_sleeping_function("{{params.delay}}")
--- dags/my_dag/main.py
+++ dags/my_dag/main.py
@@ -1,64 +1,33 @@
+import json
+import os
+import pathlib
+
 import pendulum
 from airflow import DAG
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.helpers import chain
 from hydra import compose, initialize
 from omegaconf import OmegaConf
-from airflow.utils.helpers import chain
 
 initialize(config_path="configuration")
 dags_cfg = OmegaConf.to_object(compose(config_name="dag"))
 
-with DAG(start_date=pendulum.datetime(2023, 1, 1, tz="UTC"), **dags_cfg) as dag:
+with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "configuration", "conf.json"), mode="r", encoding="utf-8") as config_file:
+    config = json.loads(config_file.read())
+
+with DAG(start_date=pendulum.datetime(2023, 1, 1, tz="UTC"), params=config, render_template_as_native_obj=True, **dags_cfg) as dag:
     empty_1 = DummyOperator(task_id="empty_1")
 
-    triggers = [
-        TriggerDagRunOperator(
-            task_id=f"trigger_{_[0]}",
-            trigger_dag_id=f"dummy_{_[0]}",
+    for key in config:
+        trigger = TriggerDagRunOperator(
+            task_id=f"trigger_{key}",
+            trigger_dag_id=key,
             wait_for_completion=True,
             execution_date="{{ds}}",
             reset_dag_run=True,
             poke_interval=10,
-            conf={"delay": _[1]},
+            conf=config[key],
         )
-        for _ in [
-            ("1", "1"),
-            ("2", "1"),
-            ("3", "1"),
-            ("4", "1"),
-            ("5", "1"),
-            ("6", "-1"),
-            ("7", "1"),
-            ("8", "1"),
-            ("9", "-1"),
-            ("10", "1"),
-            ("11", "1"),
-            ("12", "1"),
-        ]
-    ]
 
-    empty_2 = DummyOperator(task_id="empty_2", trigger_rule="none_failed_min_one_success")
-
-    chain(
-        [triggers[0], triggers[1]],
-        empty_1,
-        [
-            triggers[2],
-            triggers[3],
-        ],
-        [
-            triggers[4],
-            triggers[5],
-        ],
-        [
-            triggers[6],
-            triggers[7],
-        ],
-        [
-            triggers[8],
-            triggers[9],
-        ],
-        empty_2,
-        [triggers[10], triggers[11]],
-    )
+        empty_1 >> trigger
